3779825a949ca440c56559f4bbadca33470e3edf
egorbarinov <egor.barinov@gmail.com>
2020-10-14 13:53:13 +0300

    added code from homework to the lesson02

--- lesson02/shop/src/main/resources/db/migration/V2__Add_Admin.sql
+++ lesson02/shop/src/main/resources/db/migration/V2__Add_Admin.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (id, archive, email, name, password, role, bucket_id)
-values (1, false, 'mail@mail.ru', 'admin', 'pass', 'ADMIN', null)
+values (1, false, 'mail@mail.ru', 'admin', '$2y$12$G57k2FO3h3P1imi8oxcr9eWnA4Ng3eHwS3WM6588Oua6meRFqS1SC', 'ADMIN', null)
