80e26ec51ca5319a70d478aa49fd484e452147f7
egorbarinov <59361760+egorbarinov@users.noreply.github.com>
2020-10-20 14:05:34 +0300

    Lesson03 (#5)
    * added code from homework to the lesson02
    
    * added code from homework to the lesson03

---
+++ lesson03/shop/src/main/java/com/egorbarinov/springleveltwo/shop/service/ProductServiceImpl.java
@@ -0,0 +1,55 @@
+package com.egorbarinov.springleveltwo.shop.service;
+
+import com.egorbarinov.springleveltwo.shop.aspect.MeasureMethod;
+import com.egorbarinov.springleveltwo.shop.dao.ProductRepository;
+import com.egorbarinov.springleveltwo.shop.domain.Bucket;
+import com.egorbarinov.springleveltwo.shop.domain.User;
+import com.egorbarinov.springleveltwo.shop.dto.ProductDto;
+import com.egorbarinov.springleveltwo.shop.mapper.ProductMapper;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductMapper mapper = ProductMapper.MAPPER;
+
+    private final ProductRepository productRepository;
+    private final UserService userService;
+    private final BucketService bucketService;
+
+    public ProductServiceImpl(ProductRepository productRepository, UserService userService, BucketService bucketService) {
+        this.productRepository = productRepository;
+        this.userService = userService;
+        this.bucketService = bucketService;
+    }
+
+    @MeasureMethod
+    @Override
+    public List<ProductDto> getAll() {
+        return mapper.fromProductList(productRepository.findAll());
+    }
+
+    @Override
+    @Transactional
+    public void addToUserBucket(Long productId, String username) {
+        User user = userService.findByName(username);
+        if(user == null){
+            throw new RuntimeException("User not found. " + username);
+        }
+
+        Bucket bucket = user.getBucket();
+        if(bucket == null){
+            Bucket newBucket = bucketService.createBucket(user, Collections.singletonList(productId));
+            user.setBucket(newBucket);
+            userService.save(user);
+        }
+        else {
+            bucketService.addProducts(bucket, Collections.singletonList(productId));
+        }
+    }
+}
+
---
+++ lesson03/shop/src/main/resources/db/migration/V3__Add_Products.sql
@@ -0,0 +1,15 @@
+INSERT INTO products(
+    id, title, price)
+VALUES (1, 'Cheese', 450.75);
+
+INSERT INTO products(
+    id, title, price)
+VALUES (2, 'Milk', 50.75);
+
+INSERT INTO products(
+    id, title, price)
+VALUES (3, 'Chocolate', 123.75);
+
+INSERT INTO products(
+    id, title, price)
+VALUES (4, 'Eggs', 75.0);
