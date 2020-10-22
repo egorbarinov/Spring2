836fd682d1bee3f43c013ae405e562eeca3553a0
egorbarinov <egor.barinov@gmail.com>
2020-10-22 16:55:53 +0300

    added code from homework to the lesson05

---
+++ lesson05/cloudRouter/src/test/java/com/egorbarinov/springleveltwo/cloudRouter/CloudRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egorbarinov.springleveltwo.cloudRouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ lesson05/shop/src/main/java/com/egorbarinov/springleveltwo/shop/controller/BucketController.java
@@ -0,0 +1,41 @@
+package com.egorbarinov.springleveltwo.shop.controller;
+
+import com.egorbarinov.springleveltwo.shop.dao.BucketRepository;
+import com.egorbarinov.springleveltwo.shop.domain.Bucket;
+import com.egorbarinov.springleveltwo.shop.dto.BucketDto;
+import com.egorbarinov.springleveltwo.shop.dto.ProductDto;
+import com.egorbarinov.springleveltwo.shop.service.BucketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class BucketController {
+
+    private final BucketService bucketService;
+
+    public BucketController(BucketService bucketService) {
+        this.bucketService = bucketService;
+    }
+
+    @GetMapping("/bucket")
+    public String aboutBucket(Model model, Principal principal){
+        if(principal == null){
+            model.addAttribute("bucket", new BucketDto());
+        }
+        else {
+            BucketDto bucketDto = bucketService.getBucketByUser(principal.getName());
+            model.addAttribute("bucket", bucketDto);
+        }
+
+        return "bucket";
+    }
+
+}
