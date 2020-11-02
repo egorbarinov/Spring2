4173f8adf1c953c3e1b11cb30092654927c0c724
egorbarinov <59361760+egorbarinov@users.noreply.github.com>
2020-11-02 22:13:51 +0300

    Lesson08 (#10)
    * added homework to the lesson02
    
    * added code from homework to the lesson03
    
    * added code from homework to the lesson04
    
    * added code from homework to the lesson05
    
    * added code from homework to the lesson06
    
    * added code from homework to the lesson07
    
    * added code from homework to the lesson08

---
+++ lesson08/shop/src/main/java/com/egorbarinov/springleveltwo/shop/domain/Bucket.java
@@ -0,0 +1,30 @@
+package com.egorbarinov.springleveltwo.shop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "buckets")
+public class Bucket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToMany
+    @JoinTable(name = "bucket_products",
+            joinColumns = @JoinColumn(name = "bucket_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> products;
+}
---
+++ lesson08/shop/src/main/resources/db/migration/V2__Add_Admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (archive, email, name, password, role, bucket_id)
+values (false, 'mail@mail.ru', 'admin', '$2y$12$G57k2FO3h3P1imi8oxcr9eWnA4Ng3eHwS3WM6588Oua6meRFqS1SC', 'ADMIN', null)
